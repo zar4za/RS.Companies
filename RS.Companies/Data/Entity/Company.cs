24409d595f10a708d0ec014d7c52a4a8fbdcfaf5
zar4za <zar4z4@yandex.ru>
2023-07-19 00:44:22 +0300

    feat: initialize dbcontext with entities

---
+++ RS.Companies/Data/Entity/Company.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace RS.Companies.Data.Entity;
+
+public class Company
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string Address { get; set; } = null!; 
+    public string City { get; set; } = null!;
+    public string State { get; set; } = null!;
+    public string PhoneNumber { get; set; } = null!;
+    
+    public ICollection<Order> History { get; set; } = null!;
+    
+    [JsonIgnore]
+    public ICollection<Note> Notes { get; set; } = null!;
+    public ICollection<Person> Employees { get; set; } = null!;
+}
---
+++ RS.Companies/Data/Entity/Note.cs
@@ -0,0 +1,10 @@
+﻿namespace RS.Companies.Data.Entity;
+
+public class Note
+{
+    public int Id { get; set; }
+    public int InvoiceNumber { get; set; }
+    
+    public int CompanyId { get; set; }
+    public Company Company { get; set; } = null!;
+}
